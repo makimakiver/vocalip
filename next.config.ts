4057041b0b725dff5a88c04102c9810f285da22c
ネルフ受付係員(29) <125832648+makimakiver@users.noreply.github.com>
2025-06-16 01:33:25 +0100

    Why doesnt work?

--- next.config.ts
+++ next.config.ts
@@ -1,23 +1,18 @@
 import type { NextConfig } from "next";
+import path from "path";
+import os from "os";
 
 const nextConfig: NextConfig = {
   transpilePackages: ['@tomo-inc/tomo-evm-kit', '@tomo-wallet/uikit-lite', '@tomo-inc/shared-type'],
   webpack(config, { isServer }) {
     // 1) Ignore all `.d.ts` files so they never hit the parser:
-    config.module.rules.push({
-      test: /\.d\.ts$/,
-      use: 'ignore-loader',
-    });
+    config.cache = {
+      type: 'filesystem',
+      cacheDirectory: path.join(os.tmpdir(), 'webpack-cache'),
+    };
 
-    if (isServer) {
-      // 2) Don’t bundle these server-only Remotion modules; load them at runtime instead
-      config.externals = [
-        ...config.externals,
-        '@remotion/bundler',
-        '@remotion/renderer',
-        '@remotion/babel-loader',
-      ];
-    }
+    // 2) Make sure TS/TSX modules are resolvable
+    config.resolve.extensions.push('.ts', '.tsx');
 
     return config;
   },
